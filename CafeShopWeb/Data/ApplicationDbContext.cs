c66fa5bc37da2ab14f0766638d14d823d39bae5a
Le Gia Luan <legialuan780@gmail.com>
2025-10-30 22:50:40 +0700

    toi uu UI category, them Create

--- CafeShopWeb/Data/ApplicationDbContext.cs
+++ CafeShopWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,13 @@ namespace CafeShopWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Cafe", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Tea", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Cookie", DisplayOrder = 3 }
+                );
+        }
     }
 }
--- CafeShopWeb/Migrations/20251030140403_SeedMigrationTable.Designer.cs
+++ CafeShopWeb/Migrations/20251030140403_SeedMigrationTable.Designer.cs
@@ -3,6 +3,7 @@ using CafeShopWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CafeShopWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251030140403_SeedMigrationTable")]
+    partial class SeedMigrationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
