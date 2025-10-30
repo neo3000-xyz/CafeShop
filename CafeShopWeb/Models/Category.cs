4a3955e78fdd6583dbf8f80af88271ce663b5345
Le Gia Luan <legialuan780@gmail.com>
2025-10-30 23:58:09 +0700

    validate cho category

--- CafeShopWeb/Models/Category.cs
+++ CafeShopWeb/Models/Category.cs
@@ -8,8 +8,10 @@ namespace CafeShopWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
+        [Range(1, 100)]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
