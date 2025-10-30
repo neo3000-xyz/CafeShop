6cca62c599cfc1d7dcebdca9503852d9f347e2be
Le Gia Luan <legialuan780@gmail.com>
2025-10-30 16:51:32 +0700

    tao bang

--- CafeShopWeb/Data/ApplicationDbContext.cs
+++ CafeShopWeb/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace CafeShopWeb.Data
+﻿using CafeShopWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeShopWeb.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
