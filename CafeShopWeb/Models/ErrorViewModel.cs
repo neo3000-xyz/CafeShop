5bff8c12d3608c6115f5b9a2fbc4cdeb59df8a84
Le Gia Luan <legialuan780@gmail.com>
2025-10-30 14:22:22 +0700

    Add project files.

---
+++ CafeShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CafeShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
