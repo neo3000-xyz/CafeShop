a8b1b4c3866c36c7153c50c2521c06077aa69f74
Le Gia Luan <legialuan780@gmail.com>
2025-11-02 17:28:52 +0700

    Home product

--- CafeShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ CafeShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Diagnostics;
 using CafeShop.DataAccess.Repository.IRepository;
 using CafeShop.Models;
@@ -10,14 +11,17 @@ namespace CafeShopWeb.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
