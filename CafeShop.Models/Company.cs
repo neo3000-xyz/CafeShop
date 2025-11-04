c41112895376bedef3d202456d614502d223adaa
Le Gia Luan <legialuan780@gmail.com>
2025-11-05 00:59:30 +0700

    UI fix

--- CafeShop.Models/Company.cs
+++ CafeShop.Models/Company.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace CafeShop.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class Company
     {
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? District { get; set; }
         public string? PostalCode { get; set; }
-
+        public string? PhoneNumber { get; set; }
     }
 }
