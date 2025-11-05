aaa81aee94850eee090b25008f73afb85494a110
Le Gia Luan <legialuan780@gmail.com>
2025-11-06 01:07:12 +0700

    add model shopping cart

---
+++ CafeShop.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,28 @@
+﻿using CafeShop.DataAccess.Data;
+using CafeShop.DataAccess.Repository.IRepository;
+using CafeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeShop.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
