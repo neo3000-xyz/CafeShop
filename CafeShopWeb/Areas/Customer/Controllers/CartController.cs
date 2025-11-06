e6b0c2a85abd5c69e5f940506afdf516c02e067d
Le Gia Luan <legialuan780@gmail.com>
2025-11-07 00:41:14 +0700

    add shopping cart icon

---
+++ CafeShopWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CafeShopWeb.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
