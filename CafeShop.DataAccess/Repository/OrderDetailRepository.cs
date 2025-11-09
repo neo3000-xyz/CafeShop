9fde386ad314400f28c0cf0b3526ac5bb5b06225
Le Gia Luan <legialuan780@gmail.com>
2025-11-09 14:14:28 +0700

    them order confirm

---
+++ CafeShop.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,27 @@
+﻿using CafeShop.DataAccess.Data;
+using CafeShop.DataAccess.Repository.IRepository;
+using CafeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeShop.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
--- CafeShop.DataAccess/Repository/UnitOfWork.cs
+++ CafeShop.DataAccess/Repository/UnitOfWork.cs
@@ -17,6 +17,9 @@ namespace CafeShop.DataAccess.Repository
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
 
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
+
         public IApplicationUserRepository ApplicationUser { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
@@ -27,6 +30,8 @@ namespace CafeShop.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
 
